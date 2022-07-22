7bed5bf521a17602696aebdc3ca6e6aa7d43e9a9
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2022-07-22 14:19:16 -0300

    propriedades foram transformadas em propriedades privadas;
    Isolando o unico meio de acesso a elas pelo metodo buscarDadosDoJogo
    
    Ajuda a manter claro que tais propriedades não seram processadas fora da classe Forca

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,52 +1,56 @@
 class Forca {
+  #palavraSecreta;
+  #vida;
+  #palavra;
+  #letrasChutadas;
 
   constructor (palavraSecreta = "") {
-    this.vida = 6;
-    this.palavraSecreta =  new Array(...palavraSecreta);
-    this.palavra = new Array(palavraSecreta.length).fill('_');
-    this.letrasChutadas = [];
+    this.#palavraSecreta =  new Array(...palavraSecreta);
+    this.#vida = 6;
+    this.#palavra = new Array(palavraSecreta.length).fill('_');
+    this.#letrasChutadas = [];
   }
   
   chutar(letra) 
   {
-    if(this.letraValida(letra)) 
+    if(this.#letraValida(letra)) 
     {
-        this.letrasChutadas.push(letra);
-
-        this.letraCorreta(letra) ? this.adicionarLetraNaPalavra(letra) : this.vida--;
+      this.#letrasChutadas.push(letra);
+      this.#letraCorreta(letra) ? this.#adicionarLetraNaPalavra(letra) : this.vida--;
     }
   }
 
-  adicionarLetraNaPalavra(chute) {
-    this.palavraSecreta.forEach((l, i) => {
-      this.palavra[i] = l === chute ? chute : this.palavra[i];
-    });
-  }
-
-  letraCorreta(chute) {
-    return this.palavraSecreta.includes(chute);
-  }
-
-  letraValida(chute) {
-    return chute.length === 1 && !this.letrasChutadas.includes(chute)
-  }
-
-
   buscarEstado() {
-    if(this.palavra.join('') == this.palavraSecreta.join(''))
-      return "ganhou"
-    else if(this.vida <= 0)
-      return "perdeu"
-    else if(this.palavra !== this.palavraSecreta && this.vida > 0)
-      return "aguardando chute"
+    if(this.#palavra.join('') == this.#palavraSecreta.join(''))
+      return "ganhou";
+
+    else if(this.#vida <= 0)
+      return "perdeu";
+
+    else if(this.#palavra !== this.#palavraSecreta && this.#vida > 0)
+      return "aguardando chute";
   }
 
   buscarDadosDoJogo() {
       return {
-        letrasChutadas: this.letrasChutadas, 
-        vidas: this.vida, 
-        palavra: this.palavra 
-      }
+        letrasChutadas: this.#letrasChutadas, 
+        vidas: this.#vida, 
+        palavra: this.#palavra
+      };
+  }
+  
+  #adicionarLetraNaPalavra(chute) {
+    this.#palavraSecreta.forEach((l, i) => {
+      this.#palavra[i] = l === chute ? chute : this.#palavra[i];
+    });
+  }
+
+  #letraCorreta(chute) {
+    return this.#palavraSecreta.includes(chute);
+  }
+
+  #letraValida(chute) {
+    return chute.length === 1 && !this.#letrasChutadas.includes(chute);
   }
 }
 
